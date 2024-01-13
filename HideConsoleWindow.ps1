16b0a42168074860dbf222600d2e30f1bcd99043
Powershell Lover <151552809+EvilBytecode@users.noreply.github.com>
2024-01-13 16:17:34 +0000

    Create HideConsoleWindow.ps1

---
+++ HideConsoleWindow.ps1
@@ -0,0 +1,23 @@
+Add-Type @"
+    using System;
+    using System.Runtime.InteropServices;
+
+    public class User32 {
+        [DllImport("user32.dll")]
+        public static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+    }
+
+    public class Kernel32 {
+        [DllImport("kernel32.dll")]
+        public static extern IntPtr GetConsoleWindow();
+    }
+"@
+
+$kittyhide = 0
+
+$kittywind = [Kernel32]::GetConsoleWindow()
+Write-Host "you can see this!"
+Start-Sleep 3
+[User32]::ShowWindow($kittywind, $kittyhide)
+Write-Host "you cannot see this!"
+Start-Sleep 15
